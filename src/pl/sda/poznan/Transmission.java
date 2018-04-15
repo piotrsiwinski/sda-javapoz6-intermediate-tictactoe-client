5faad41c7c828232a8862e5cfd0c4d1e394d4609
Piotr Siwinski <piotr.siwinski@f-secure.com>
2018-04-15 16:02:02 +0200

    Added connection to server

---
+++ src/pl/sda/poznan/Transmission.java
@@ -0,0 +1,34 @@
+package pl.sda.poznan;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Transmission {
+
+  private Socket socket;
+  private ObjectOutputStream outputStream;
+  private ObjectInputStream inputStream;
+
+  public Transmission(Socket socket) throws IOException {
+    this.socket = socket;
+//    this.inputStream = new ObjectInputStream(socket.getInputStream());
+//    this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+  }
+
+  public void sendObject(Object obj) throws IOException {
+    if (outputStream == null) {
+      outputStream = new ObjectOutputStream(socket.getOutputStream());
+    }
+    outputStream.writeObject(obj);
+    outputStream.flush();
+  }
+
+  public Object readObject() throws IOException, ClassNotFoundException {
+    if (inputStream == null) {
+      inputStream= new ObjectInputStream(socket.getInputStream());
+    }
+    return inputStream.readObject();
+  }
+}
